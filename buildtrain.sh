1a57fdc2ab5a64e6da2ca7ece671bd5e49552b1d
dmitryleshchinskiy <dmitry.leshchinskiy@fr.clara.net>
2022-08-01 15:39:18 +0200

    commit

--- buildtrain.sh
+++ buildtrain.sh
@@ -12,5 +12,5 @@ echo $r
 #aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
 #docker push ${account}.dkr.ecr.${region}.amazonaws.com/train
 docker tag ${r} dmitrylesh/train:latest
-docker login --password="${pass}" --login=dmitrylesh
+docker login -p="${pass}" -u=dmitrylesh
 docker push dmitrylesh/train:latest
